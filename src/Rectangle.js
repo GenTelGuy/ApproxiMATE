c4e6529bc52513f455a1286723c580c0c735937f
Alex Medwid <alex.medwid@gmail.com>
2015-02-08 07:05:40 -0800

    Added most of the Rectangle pseudo-class

--- src/Rectangle.js
+++ src/Rectangle.js
@@ -5,6 +5,10 @@ var Rectangle = function(width, height, centerX, centerY){
 	this.height = height;
 	this.centerX = centerX;
 	this.centerY = centerY;//The position and dimensions of the rectangle
+	
+	this.area = this.width * this.height;
+	
+	this.perimeter = this.width * 2 + this.height * 2;
 
 
 
@@ -21,6 +25,24 @@ Rectangle.prototype =
 	
 	draw: function( canvas )
 	{
+	
+		var topY = centerY - this.height/2;//Determines the positions of the rectangle's edges
+		var bottomY = centerY + this.height/2;
+		
+		var leftX = centerX - this.width/2;
+		var rightX = centerX + this.width/2;
+		
+		
+		//This draws it on screen
+		canvas.strokeStyle = "black";
+		canvas.beginPath();
+		canvas.moveTo(leftX, topY); //the initial point
+		canvas.lineTo(rightX, topY);
+		canvas.lineTo(rightX, bottomY);
+		canvas.lineTo( leftX, bottomY);
+		canvas.closePath();
+		canvas.stroke(); //draws an outline
+		
 	
 	
 	}
