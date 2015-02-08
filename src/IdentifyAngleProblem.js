c090bd770c8073d42c245d5912e4317fb0434f99
Alex Medwid <alex.medwid@gmail.com>
2015-02-07 20:49:26 -0800

    Added incomplete functions for deciding the angles

--- src/IdentifyAngleProblem.js
+++ src/IdentifyAngleProblem.js
@@ -5,11 +5,119 @@ var IdentifyAngleProblem = function(x, y)
     this.x = x;
 	this.y = y;
 	
-	this.crossY = 128; //the top y-position of the cross drawn
+	this.minimumAngle = 1;//One degree is the minimum that the angle can be
+	this.maximumAngle = 359;//One less than 360 degrees. It is likely that these numbers need adjustment.
 	
-	this.theta = Math.floor((Math.random() * 360) + 1); 
-	this.length = 76; //the length of the rays that make up the angle
+	this.targetAngle = Math.floor(Math.random() * (this.maximum-this.minimum) + this.minimum);
+	//This line sets the angle that the user tries to identify
+	
+	this.choice1 = 0;
+	this.choice2 = 0;
+	this.choice3 = 0;
+	
+	switch(chooseValueBetween(1, 3)){
+	
+		case 1:
+		
+			this.choice1 = this.targetAngle;
+			
+			//Set the other two answers to other, incorrect angles
+			
+			
+			break;
+		
+		case 2:
+		
+			this.choice2 = this.targetAngle;
+			
+			//Set the other two answers to other, incorrect angles
+		
+		
+			break;
+			
+		default: //Case 3
+		
+			this.choice3 = this.targetAngle;
+			
+			//Set the other two answers to other, incorrect angles
+			
+			
+			
+			break;
+			
+	
+	}
 }
+
+
+var generateWrongAnswer = function( correctAnswer, minimumDifference, maximumDifference, min, max ) //Minimum difference is the minimum difference between the correct angle and an incorrect angle.
+{
+
+	switch( chooseValueBetween(1, 2) ){
+	
+		case 1://Go low
+		
+		
+			this.ret = chooseValueBetween( correctAnswer - maximumDifference, correctAnswer - minimumDifference ) ;
+			
+			if(this.ret > max){
+			
+			this.ret = max;
+			}
+			
+			if(this.ret < min) {
+			
+			this.ret = min
+			};
+			
+			return this.ret;
+			
+			
+			break;
+		
+		case 2://Go high 
+		
+			this.ret =  chooseValueBetween( correctAnswer + minimumDifference, correctAnswer + maximumDifference ) ;
+			if(this.ret > max){//Make sure you don't go too high or too low
+			
+				this.ret = max;
+			}
+			
+			if(this.ret < min) {//^^
+			
+				this.ret = min
+			};
+		
+			return this.ret;
+			break;
+		
+		default: // do nothing, this one never happens
+	
+	}
+	
+	
+
+
+
+}
+
+var generateWrongAnswer = function( correctAnswer, minimumDifference, maximumDifference, min, max, alreadyUsedWrongAnswer ) //Overloaded function, avoids two of the wrong answers being the same
+{
+
+
+
+
+}
+
+var chooseValueBetween = function( min, max )//Chooses a random value between the min and the max, inclusive.
+{
+
+return( Math.floor(  Math.random() * (1 + min-max) + min  ) );
+
+
+}
+
+
 
 IdentifyAngleProblem.prototype =
 {
@@ -20,24 +128,6 @@ IdentifyAngleProblem.prototype =
 
     draw: function(canvas)
     {
-		canvas.fillRect(engine.w / 2, this.crossY, 1, engine.h); //vertical line
-		canvas.fillRect(0, (this.crossY + engine.h) / 2, engine.w, 1); //horizontal line
 		canvas.fillText("This is an identify-angle problem", this.x, this.y);
-		
-		//draws the generated angle
-		var angleX = (engine.w / 2) + (this.length + 12);
-		var angleY = (this.crossY + engine.h) / 2 - (this.length + 12);
-		canvas.beginPath();
-		canvas.moveTo(angleX, angleY); //the initial point
-		canvas.lineTo(angleX + this.length, angleY);
-		canvas.lineTo(angleX, angleY);
-		canvas.lineTo(angleX + Math.cos(this.theta  * Math.PI / 180) * this.length, angleY - Math.sin(this.theta  * Math.PI / 180) * this.length);
-		canvas.closePath();
-		canvas.stroke(); //draws an outline
-    },
-	
-	//Utility function so that the angle can be generated at any time by calling this function.
-	generateAngle: function(){
-		this.theta = Math.floor((Math.random() * 360) + 1);
-	}
+    }
 }
